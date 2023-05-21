cdd6f24d010e41496cd24e80404e00827641e970
stefan-dekkers <stefandekkers2005@gmail.com>
2023-05-21 23:51:07 +0200

    'V4'

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -7,26 +7,28 @@ const jwt = require('jsonwebtoken');
 
 const { emailValidation, passwordValidation, phoneNumberValidation } = require('./validation');
 
+function validateLogin(req, res, next) {
+    try {
+        assert(req.body.emailAddress != null, 'emailAddress is missing');
+        assert(typeof req.body.emailAddress === 'string', 'emailAddress must be a string.');
+        emailValidation(req.body.emailAddress);
+        assert(req.body.password != null, 'password is missing');
+        assert(typeof req.body.password === 'string', 'password must be a string.');
+        next();
+    } catch (err) {
+        next({
+            status: 400,
+            message: err.message,
+            data: {},
+        });
+        return;
+    }
+}
+
 const authController = {
     // UC-101: Login
     login: (req, res, next) => {
         logger.info('UC-101: Login');
-
-        try {
-            assert(req.body.emailAddress != null, 'emailAddress is missing');
-            assert(typeof req.body.emailAddress === 'string', 'emailAddress must be a string.');
-            emailValidation(req.body.emailAddress);
-            assert(req.body.password != null, 'password is missing');
-            assert(typeof req.body.password === 'string', 'password must be a string.');
-            next();
-        } catch (err) {
-            next({
-                status: 400,
-                message: err.message,
-                data: {},
-            });
-            return;
-        }
 
         const { emailAddress, password } = req.body;
 
@@ -44,53 +46,64 @@ const authController = {
                 return;
             }
             if (conn) {
-                // Check if user exists
-                conn.query(query, values, (err, results, fields) => {
+                validateLogin(req, res, (err) => {
                     if (err) {
                         next({
-                            status: 409,
-                            message: [err.code, err.message],
-                            data: {},
-                        });
-                        return;
-                    }
-
-                    if (results.length === 0) {
-                        next({
-                            status: 404,
-                            message: 'User does not exist',
-                            data: {},
-                        });
-                        return;
-                    }
-
-                    const user = results[0];
-
-                    // Check password
-                    if (password === user.password) {
-                        const payload = {
-                            userId: user.id,
-                        };
-
-                        // Generate the bearer token
-                        jwt.sign(payload, jwtSecretKey, { expiresIn: '2d' }, (err, token) => {
-                            logger.debug(`User logged in, sending: ${user}`);
-                            res.status(200).json({
-                                status: 200,
-                                message: 'User logged in',
-                                data: { ...user, token },
-                            });
-                        });
-                    } else {
-                        next({
                             status: 400,
-                            message: 'Not authorized',
+                            message: err.code,
                             data: {},
                         });
                         return;
                     }
 
-                    pool.releaseConnection(conn);
+                    // Check if user exists
+                    conn.query(query, values, (err, results, fields) => {
+                        if (err) {
+                            next({
+                                status: 409,
+                                message: [err.code, err.message],
+                                data: {},
+                            });
+                            return;
+                        }
+
+                        if (results.length === 0) {
+                            next({
+                                status: 404,
+                                message: 'User does not exist',
+                                data: {},
+                            });
+                            return;
+                        }
+
+                        const user = results[0];
+
+                        // Check password
+                        if (password === user.password) {
+                            const payload = {
+                                userId: user.id,
+                            };
+
+                            // Generate the bearer token
+                            jwt.sign(payload, jwtSecretKey, { expiresIn: '2d' }, (err, token) => {
+                                logger.debug(`User logged in, sending: ${user}`);
+                                res.status(200).json({
+                                    status: 200,
+                                    message: 'User logged in',
+                                    data: { ...user, token },
+                                });
+                            });
+                        } else {
+                            next({
+                                status: 400,
+                                message: 'Not authorized',
+                                data: {},
+                            });
+                            return;
+                        }
+
+                        pool.releaseConnection(conn);
+                    });
                 });
             }
         });
